3d33cc908709c1e29baff542ac83980932987ea6
Jeff Mitchell <jeff_mitchell_dev@icloud.com>
2025-04-02 19:42:42 -0700

    improvement:implement conversion trait to convert incoming data to domain data

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,8 +1,10 @@
 mod blueprint;
 pub mod configuration;
+pub mod errors;
 pub mod models;
 pub mod routes;
 pub mod telemetry;
 
 pub use blueprint::blueprint;
+pub use errors::*;
 pub use models::*;
