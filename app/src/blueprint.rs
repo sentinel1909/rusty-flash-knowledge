19410ffbd9b26e4d0872f8f6bde181ddf87b88c5
Jeff Mitchell <jeff_mitchell_dev@icloud.com>
2025-05-10 10:17:38 -0700

    improvement: use pavex_session_sqlx

--- app/src/blueprint.rs
+++ app/src/blueprint.rs
@@ -2,16 +2,18 @@
 
 // dependencies
 use crate::{configuration, routes, telemetry};
+use pavex::f;
 use pavex::blueprint::Blueprint;
 use pavex::cookie::CookieKit;
-use pavex::f;
 use pavex::kit::ApiKit;
+use pavex_session_sqlx::PostgresSessionKit;
 
 /// The main blueprint, containing all the routes, middlewares, constructors and error handlers
 /// required by our API.
 pub fn blueprint() -> Blueprint {
     let mut bp = Blueprint::new();
     ApiKit::new().register(&mut bp);
+    PostgresSessionKit::new().register(&mut bp);
     CookieKit::new().register(&mut bp);
     telemetry::register(&mut bp);
     configuration::register(&mut bp);
