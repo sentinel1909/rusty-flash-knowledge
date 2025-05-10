eaedb6d9352814c2109808b8ba12e8c62e1c4723
Jeff Mitchell <jeff_mitchell_dev@icloud.com>
2025-05-09 20:12:42 -0700

    chore: add machinery for cookies and sessions

--- app/src/blueprint.rs
+++ app/src/blueprint.rs
@@ -3,6 +3,8 @@
 // dependencies
 use crate::{configuration, routes, telemetry};
 use pavex::blueprint::Blueprint;
+use pavex::cookie::CookieKit;
+use pavex_session_memory_store::InMemorySessionKit;
 use pavex::f;
 use pavex::kit::ApiKit;
 
@@ -11,6 +13,8 @@ use pavex::kit::ApiKit;
 pub fn blueprint() -> Blueprint {
     let mut bp = Blueprint::new();
     ApiKit::new().register(&mut bp);
+    InMemorySessionKit::new().register(&mut bp);
+    CookieKit::new().register(&mut bp);
     telemetry::register(&mut bp);
     configuration::register(&mut bp);
 
