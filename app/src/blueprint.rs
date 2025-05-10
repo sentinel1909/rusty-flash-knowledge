9f177b9f924fc0a63c0d92b7e9e0900927810d05
Jeff Mitchell <jeff_mitchell_dev@icloud.com>
2025-05-10 08:55:59 -0700

    chore: prepare for redis, remove in memory store session

--- app/src/blueprint.rs
+++ app/src/blueprint.rs
@@ -4,7 +4,6 @@
 use crate::{configuration, routes, telemetry};
 use pavex::blueprint::Blueprint;
 use pavex::cookie::CookieKit;
-use pavex_session_memory_store::InMemorySessionKit;
 use pavex::f;
 use pavex::kit::ApiKit;
 
@@ -13,7 +12,6 @@ use pavex::kit::ApiKit;
 pub fn blueprint() -> Blueprint {
     let mut bp = Blueprint::new();
     ApiKit::new().register(&mut bp);
-    InMemorySessionKit::new().register(&mut bp);
     CookieKit::new().register(&mut bp);
     telemetry::register(&mut bp);
     configuration::register(&mut bp);
